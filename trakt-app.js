8540fcfbb8cf9bce837c968b4da97491c788c498
Sankalp Singh <sankalpsingh12345@gmail.com>
2023-10-25 16:52:34 +0530

    added the js file and modified some css

--- trakt-app.js
+++ trakt-app.js
@@ -1,1 +1,7 @@
-const apiKey = 
+//main js code for the weather-trakt app
+
+const apiKey = "059efa9e9bc9c515031e2f68eb49d4a5";
+const apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+
+fetch(apiUrl)
+.then(response => response.json)
